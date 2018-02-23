2be71331cf94b58b932b9fdbd142e4a31badfcdb
Anand Sonawane <avsonawane28@gmail.com>
2018-02-23 16:03:18 +0530

    adding more files

--- Clustering/accuracy_agglomerative.py
+++ Clustering/accuracy_agglomerative.py
@@ -18,8 +18,8 @@ for cluster_number in range(2,60):
         items_counts = df2['Category'].value_counts()
         max_item = items_counts[0]   ## highest value from a particular cluster
         count += max_item
-    accuracy = float(count*100)/len
-    acc[cluster_number] = accuracy
+    	accuracy = float(count*100)/len
+    	acc[cluster_number] = accuracy
 df_acc = pd.DataFrame(acc.items(), columns=['num_of_cluster', 'Accuracy'], index = None)
 df_acc.to_csv(dir_path+'/ACCURACY_agglomerative.csv')
 
