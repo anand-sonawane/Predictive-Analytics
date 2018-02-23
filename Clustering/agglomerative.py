72a415b8a2ef537db52eecea368ca5dfddbfb311
Anand Sonawane <avsonawane28@gmail.com>
2018-02-23 12:11:04 +0530

    Adding clustering

---
+++ Clustering/agglomerative.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import time
+import os
+import scipy
+from scipy import io
+from sklearn.cluster import AgglomerativeClustering
+import threading
+
+dir_path = os.getcwd()
+feature_data = pd.read_csv("avs.csv")
+feature_data = feature_data.drop(['Unnamed: 0'], axis=1)
+
+start_time = time.time()
+label = {}
+
+def clustering(label,num_cluster):
+    for i in range(2,num_cluster):
+        print(i)
+        start_time = time.time()
+        hclustering = AgglomerativeClustering(n_clusters = i)
+        hclustering.fit(feature_data)
+        label[i] = list(hclustering.labels_)
+    label['Category'] = actual_location
+    label['Frame_Name'] = img_name
+    df = pd.DataFrame(label)
+    df.to_csv(dir_path+'/CLUSTERS_Agglomerative.csv')
+
+threads = []
+for i in range(2,61):
+    thread = threading.Thread(target=clustering,args=(label,i))
+    threads.append(thread)
+
+for st in threads:
+    st.start()
+
+print(time.time() - start_time)
+
